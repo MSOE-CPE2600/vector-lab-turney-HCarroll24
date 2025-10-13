58aa206413136ecd1abc9899124b7748a29ac9aa
Hunter Carroll <carrollh@msoe.edu>
2025-10-13 17:02:43 -0500

    Initial Version

---
+++ user_interface.h
@@ -0,0 +1,22 @@
+/********************************************************************
+ * @file user_interface.h
+ * @brief declarations for the user interface
+ *
+ * Name: Hunter Carroll
+ * Course: CPE2600
+ * Section: 012
+ * Assignent: Lab 5
+********************************************************************/
+#ifndef USER_INTERFACE_H
+#define USER_INTERFACE_H
+
+/**
+ * @brief Run the user interface module
+ * @param store pointer to vector store
+ * @return 0 to exit
+*/
+int run_user_interface();
+
+/* internal helpers are defined static in user_interface.c */
+
+#endif
