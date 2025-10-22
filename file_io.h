65c48cfe041878b9b35568e7b16beb17878cbfbf
Hunter Carroll <carrollh@msoe.edu>
2025-10-22 10:48:06 -0500

    Adding function headers in file_io.h

--- file_io.h
+++ file_io.h
@@ -12,8 +12,20 @@
 
 #include "vector.h"
 
+/**
+ * @brief loads vectors from a file
+ * @param filename name of file to load from
+ * @return pointer to vector list
+*/
 vector* load(char* filename);
 
+/**
+ * @brief saves vectors to a file
+ * @param filename name of file to save to
+ * @param vector_list pointer to vector list
+ * @param size size of vector list
+ * @return 0 if successful, 1 if error
+*/
 int save(char* filename, vector* vector_list, int size);
 
 #endif
